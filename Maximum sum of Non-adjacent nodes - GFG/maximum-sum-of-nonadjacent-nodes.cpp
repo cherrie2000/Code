bc9f8a252194eb6216af85bf6038fb0dd722e743
Cherri Verma <83605900+cherrie2000@users.noreply.github.com>
2023-03-29 02:51:36 +0530

    Added solution - LeetHub

--- Maximum sum of Non-adjacent nodes - GFG/maximum-sum-of-nonadjacent-nodes.cpp
+++ Maximum sum of Non-adjacent nodes - GFG/maximum-sum-of-nonadjacent-nodes.cpp
@@ -104,22 +104,20 @@ struct Node
 class Solution{
   public:
     //Function to return the maximum sum of non-adjacent nodes.
-    pair<int,int> solve(Node* root){
-        if(root== NULL){
-            pair<int,int> p=make_pair(0,0); return p;
-        }
-        pair<int,int>left = solve(root->left);
-        pair<int, int> right = solve(root->right);
-        pair<int ,int> ans;
-        ans.first = left.second+right.second+root->data;
-        ans.second= max(left.first,left.second)+max(right.first,right.second);
+    pair<int,int> solve(Node*root){
+        if(!root) return {0,0};
+        pair<int,int> left=solve(root->left);
+        pair<int,int> right=solve(root->right);
+        pair<int,int> ans;
+        ans.first=left.second+right.second+root->data;
+        ans.second=max(left.first,left.second)+max(right.first,right.second);
         return ans;
     }
     int getMaxSum(Node *root) 
     {
         // Add your code here
-        pair<int,int> res=solve(root);
-        return max(res.first,res.second);
+        pair<int,int> ans=solve(root);
+        return max(ans.first,ans.second);
     }
 };
 
